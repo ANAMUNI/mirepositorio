ff706b45d0eb2071e8cc460bd750f601d3c75f2e
ANAMUNI <59970646+ANAMUNI@users.noreply.github.com>
2020-04-13 08:26:59 +0200

    Add files via upload

---
+++ basico31mascotas3/ventana_list_widget.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ventana_list_widget.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setStyleSheet("background-color: rgb(200, 200, 194);\n"
+"background-color: rgb(200, 133, 142);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(230, 90, 241, 51))
+        font = QtGui.QFont()
+        font.setFamily("Aharoni")
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.list_widget = QtWidgets.QListWidget(self.centralwidget)
+        self.list_widget.setGeometry(QtCore.QRect(40, 180, 631, 381))
+        font = QtGui.QFont()
+        font.setFamily("Aharoni")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.list_widget.setFont(font)
+        self.list_widget.setStyleSheet("background-color: rgb(255, 217, 218);\n"
+"border-color: rgba(0, 0, 0, 249);\n"
+"border-color: rgb(0, 0, 0);\n"
+"background-color: rgb(248, 237, 237);")
+        self.list_widget.setObjectName("list_widget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "LISTADO LIST WIDGET"))
+
---
+++ basico31mascotas3/ventana_listado_accesorios.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ventana_listado_accesorios.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        MainWindow.setFont(font)
+        MainWindow.setStyleSheet("background-color: rgb(200, 149, 167);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(290, 100, 251, 71))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setStyleSheet("border-color: rgb(255, 116, 153);")
+        self.label.setObjectName("label")
+        self.listado_accesorios = QtWidgets.QTextEdit(self.centralwidget)
+        self.listado_accesorios.setGeometry(QtCore.QRect(60, 210, 691, 401))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.listado_accesorios.setFont(font)
+        self.listado_accesorios.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"background-color: rgb(255, 248, 213);")
+        self.listado_accesorios.setObjectName("listado_accesorios")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "LISTADO DE ACCESORIOS"))
+
